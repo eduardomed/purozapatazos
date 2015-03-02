2ea9192f4835958630b67df4324c1c24625e1492
Eduardo <eduardo@udacity.com>
2015-03-01 21:30:10 -0800

    Added zapatazo-specific URLs for initial load

--- application.py
+++ application.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, jsonify, request
 from database import init_db, db_session
 from models import Zapatazo
 
-app = Flask(__name__)
+app = Flask('purozapatazos')
+# static_folder='purozapatazos/static'
 init_db()
 
 
@@ -10,7 +11,7 @@ init_db()
 def main_page():
 	#TODO
 	zapatazos = db_session.query(Zapatazo).limit(4)
-	# print "zapatazos: ", zapatazos
+	print "zapatazos: ", zapatazos
 	return render_template('index.html', zapatazos = zapatazos)
 	
 @app.route('/load_more.json')
@@ -24,6 +25,13 @@ def load_more():
 @app.route('/acerca/')
 def purozapatazos():
 	return render_template('acerca.html')
+
+@app.route('/<the_title>/')
+def unzapatazo(the_title):
+	#TODO - query for specific zapatazo and return 
+	zapatazo = db_session.query(Zapatazo).filter(Zapatazo.title == the_title).first()
+	print "ZAPATAZO: ", zapatazo
+	return render_template('unzapatazo.html', zapatazo = zapatazo)
 
 if __name__ == '__main__':
     app.debug = True
