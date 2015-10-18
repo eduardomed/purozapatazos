9f178dc87063d4e6bd7ff9b148bb30edc553f7e4
Eduardo <eduardo@udacity.com>
2015-10-17 20:14:08 -0700

    Removed modal, cleaned up a bit

--- static/js/loader.js
+++ static/js/loader.js
@@ -12,9 +12,9 @@ $(document).ready(function() {
         }
     });
 
-    $('#zapatazoModal').on('show.bs.modal', function(event) {
-        openModal(event)
-    })
+    // $('#zapatazoModal').on('show.bs.modal', function(event) {
+    //     openModal(event)
+    // })
 
 });
     
@@ -27,10 +27,7 @@ function loadMore(){
         for (var index in jd["Zapatazo"]){
             var zapatazo = jd["Zapatazo"][index]
             var tile = $("<div></div>").addClass("col-md-3")
-            var link = $("<a></a>").attr("data-toggle", "modal")
-                                    .attr("data-target", "#zapatazoModal")
-                                    .data("zapatazo-id", zapatazo["id"])
-                                    .data("zapatazo-img", zapatazo["imglink"])
+            var link = $("<a></a>").attr("href",zapatazo["id"]);
             $("<img>").attr("src",  zapatazo["imglink"]).attr("alt", zapatazo["id"] ).appendTo(link)
             $(link).appendTo(tile)
             $('#zapatazos').append(tile)
@@ -40,30 +37,17 @@ function loadMore(){
 }
 
 
-function openModal(event){
-    console.log("in the event handler");
-    var button = $(event.relatedTarget) // Button that triggered the modal
+// function openModal(event){
+//     console.log("in the event handler");
+//     var button = $(event.relatedTarget) // Button that triggered the modal
 
-    // Extract info from data-* attributes
-    var zapatazoID = button.data('zapatazo-id') 
-    var zapatazoImage = button.data('zapatazo-img') 
+//     // Extract info from data-* attributes
+//     var zapatazoID = button.data('zapatazo-id') 
+//     var zapatazoImage = button.data('zapatazo-img') 
 
-    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-    var modal = $("#zapatazoModal")
-    modal.find("#zapatazo-modal-img").attr('src', zapatazoImage)
-    modal.find("#zapatazo-modal-link").attr('href', "/" + zapatazoID)
-    console.log(modal.find(".twitter-hashtag-button"));
-    modal.find(".twitter-hashtag-button").attr('data-url', "http://zapatazos.herokuapp.com/" + zapatazoID);
-    console.log(modal.find(".twitter-hashtag-button"));
-    
-    // twttr.widgets.load(button);
-    // twttr.events.bind(
-    //   'loaded',
-    //   function (event) {
-    //     event.widgets.load();
-    //   }
-    // );
-
-}
+//     var modal = $("#zapatazoModal")
+//     modal.find("#zapatazo-modal-img").attr('src', zapatazoImage)
+//     modal.find("#zapatazo-modal-link").attr('href', "/" + zapatazoID)
+//     modal.find(".twitter-hashtag-button").attr('data-url', "http://zapatazos.herokuapp.com/" + zapatazoID);
+// }
   
